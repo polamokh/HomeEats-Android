83ff512db80af80c3159d603483a261114924cdd
Pola Mikhail <pola.mikhail@engineer.com>
2020-05-12 22:30:10 +0200

    Fixed orderItem constructor

--- app/src/main/java/com/example/homeeats/Models/CartOrderItem.java
+++ app/src/main/java/com/example/homeeats/Models/CartOrderItem.java
@@ -7,8 +7,9 @@ public class CartOrderItem extends OrderItem {
     public String id;
     public String foodBuyerId;
 
-    public CartOrderItem(String id, String foodBuyerId, String mealItemId, Integer quantity, String notes, Integer rating) {
-        super(mealItemId, quantity, notes, rating);
+    public CartOrderItem(String id, String foodBuyerId, String mealItemId, Integer quantity,
+                         String notes, Integer rating, Double totalPrice) {
+        super(mealItemId, quantity, notes, rating, totalPrice);
         this.id = id;
         this.foodBuyerId = foodBuyerId;
     }
