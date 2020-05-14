d4d00ac09241c4d34b1e095312021ae81e93b8d1
Pola Mikhail <pola.mikhail@engineer.com>
2020-05-14 16:49:34 +0200

    Add cart checkout function

--- app/src/main/java/com/example/homeeats/Activities/FoodBuyer/FoodBuyerCartFragment.java
+++ app/src/main/java/com/example/homeeats/Activities/FoodBuyer/FoodBuyerCartFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,20 +15,31 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.firbasedao.Listeners.RetrievalEventListener;
+import com.example.firbasedao.Listeners.TaskListener;
 import com.example.homeeats.Adapters.FoodBuyerCartOrdersRecyclerAdapter;
 import com.example.homeeats.Dao.CartOrderItemDao;
+import com.example.homeeats.Dao.DeliveryBoyDao;
 import com.example.homeeats.Dao.FoodBuyerDao;
 import com.example.homeeats.Dao.OrderDao;
 import com.example.homeeats.Models.CartOrderItem;
+import com.example.homeeats.Models.DeliveryBoy;
 import com.example.homeeats.Models.FoodBuyer;
 import com.example.homeeats.Models.Order;
+import com.example.homeeats.Models.OrderItem;
+import com.example.homeeats.Models.OrderStatus;
 import com.example.homeeats.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class FoodBuyerCartFragment extends Fragment {
+    private HashMap<String, List<CartOrderItem>> orders;
+    private ArrayList<String> foodMakerKeys;
+
+    private FoodBuyerCartOrdersRecyclerAdapter adapter;
+
     private TextView textViewTotalPrice;
     private TextView textViewTotalPriceLabel;
     private Button buttonCheckout;
@@ -49,33 +61,33 @@ public class FoodBuyerCartFragment extends Fragment {
                 .getString("FoodBuyerID"), new RetrievalEventListener<HashMap<String, List<CartOrderItem>>>() {
             @Override
             public void OnDataRetrieved(HashMap<String, List<CartOrderItem>> stringListHashMap) {
-                ArrayList<String> foodMakerKeys = new ArrayList<>(stringListHashMap.keySet());
+                orders = stringListHashMap;
+                foodMakerKeys = new ArrayList<>(orders.keySet());
 
-                FoodBuyerCartOrdersRecyclerAdapter adapter =
-                        new FoodBuyerCartOrdersRecyclerAdapter(stringListHashMap,
-                                foodMakerKeys, FoodBuyerCartFragment.this);
+                adapter = new FoodBuyerCartOrdersRecyclerAdapter(orders,
+                        foodMakerKeys, FoodBuyerCartFragment.this);
                 recyclerView.setAdapter(adapter);
 
-                recalculateTotalPrice(stringListHashMap, foodMakerKeys);
+                recalculateTotalPrice(orders, foodMakerKeys);
             }
         });
 
         buttonCheckout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                checkoutOrders();
+            public void onClick(final View v) {
+                checkoutOrders(v);
             }
         });
 
         return view;
     }
 
-    public void recalculateTotalPrice(HashMap<String, List<CartOrderItem>> stringListHashMap,
+    public void recalculateTotalPrice(HashMap<String, List<CartOrderItem>> orders,
                                       List<String> foodMakerKeys) {
         if (foodMakerKeys.size() > 0) {
             Double ordersTotalPrice = 0.0;
             for (int i = 0; i < foodMakerKeys.size(); i++) {
-                for (CartOrderItem cartOrderItem : stringListHashMap.get(foodMakerKeys.get(i))) {
+                for (CartOrderItem cartOrderItem : orders.get(foodMakerKeys.get(i))) {
                     ordersTotalPrice += cartOrderItem.totalPrice;
                 }
             }
@@ -92,7 +104,83 @@ public class FoodBuyerCartFragment extends Fragment {
         }
     }
 
-    private void checkoutOrders() {
-        //TODO: Add on checkout click
+    private void checkoutOrders(final View v) {
+        for (int i = 0; i < foodMakerKeys.size(); i++) {
+            final Order order = new Order();
+            order.foodMakerId = foodMakerKeys.get(i);
+            order.foodBuyerId = getActivity().getIntent().getExtras().getString("FoodBuyerID");
+            order.orderItems = new ArrayList<>();
+            order.review = "";
+            order.rating = 1;
+            order.totalPrice = 0.0;
+            order.orderStatus = OrderStatus.Pending;
+
+            DeliveryBoyDao.GetInstance().GetAvailableDeliveryBoy(
+                    new RetrievalEventListener<DeliveryBoy>() {
+                        @Override
+                        public void OnDataRetrieved(DeliveryBoy deliveryBoy) {
+                            if (deliveryBoy != null) {
+                                order.deliveryBoyId = deliveryBoy.id;
+                                deliveryBoy.availability = false;
+                                DeliveryBoyDao.GetInstance().save(
+                                        deliveryBoy, deliveryBoy.id, new TaskListener() {
+                                            @Override
+                                            public void OnSuccess() {
+                                            }
+
+                                            @Override
+                                            public void OnFail() {
+                                            }
+                                        }
+                                );
+                            }
+                            else {
+                                Toast.makeText(v.getContext(),
+                                        "No available delivery boys",
+                                        Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+
+                            FoodBuyerDao.GetInstance().get(order.foodBuyerId,
+                                    new RetrievalEventListener<FoodBuyer>() {
+                                        @Override
+                                        public void OnDataRetrieved(FoodBuyer foodBuyer) {
+                                            order.buyerLocation = foodBuyer.location;
+
+                                            int mealsSize = orders.get(order.foodMakerId).size();
+                                            for (int j = 0; j < mealsSize; j++) {
+                                                order.orderItems.add(orders.get(order.foodMakerId).get(j));
+                                            }
+                                            OrderDao.GetInstance().save(order, OrderDao.GetInstance().GetNewKey(),
+                                                    new TaskListener() {
+                                                        @Override
+                                                        public void OnSuccess() {
+                                                            CartOrderItemDao.GetInstance()
+                                                                    .removeFoodBuyerCart(
+                                                                            order.foodBuyerId
+                                                                    );
+
+                                                            orders.clear();
+                                                            foodMakerKeys.clear();
+                                                            adapter.notifyDataSetChanged();
+
+                                                            recalculateTotalPrice(orders,
+                                                                    foodMakerKeys);
+
+
+                                                            Toast.makeText(v.getContext(),
+                                                                    "Your orders made successfully",
+                                                                    Toast.LENGTH_SHORT).show();
+                                                        }
+
+                                                        @Override
+                                                        public void OnFail() {
+                                                        }
+                                                    });
+                                        }
+                                    });
+                        }
+                    });
+        }
     }
 }
